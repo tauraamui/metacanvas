959d553a0a220093f4fb441b5967ccd1d844ebab
tauraamui <adampstringer@protonmail.com>
2022-02-07 22:53:22 +0000

    Separate panning and scaling value update and render calls

--- meta/canvas.go
+++ meta/canvas.go
@@ -1,6 +1,8 @@
 package meta
 
 import (
+	"image"
+
 	"gioui.org/f32"
 	"gioui.org/font/gofont"
 	"gioui.org/io/pointer"
@@ -13,6 +15,7 @@ type Canvas struct {
 	theme   *material.Theme
 	page    *page
 	input   *input
+	scale   float32
 	offsetX float32
 	offsetY float32
 }
@@ -21,28 +24,50 @@ func NewCanvas() *Canvas {
 	return &Canvas{
 		input: &input{pointerEventTag: struct{}{}},
 		theme: material.NewTheme(gofont.Collection()),
+		scale: 1,
 		page:  NewA4(),
 	}
 }
 
 func (c *Canvas) Render(gtx layout.Context) {
-	defer c.applyTransforms(gtx).Pop()
+	c.updateTransforms(gtx)
+	c.applyTransforms(gtx)
 	c.page.Render(gtx.Ops)
 }
 
-func (c *Canvas) applyTransforms(gtx layout.Context) op.TransformStack {
+func (c *Canvas) updateTransforms(gtx layout.Context) {
 	c.input.update(gtx)
+
 	if c.input.drag {
 		c.offsetX -= c.input.dragDeltaX
 		c.offsetY -= c.input.dragDeltaY
+		return
 	}
-	transforms := op.Offset(f32.Pt(c.offsetX, c.offsetY)).Push(gtx.Ops)
-	return transforms
+
+	if c.input.scroll {
+		c.scale -= c.input.scrollY
+		if c.scale > 1 {
+			c.scale = 1
+		} else if c.scale < 0.01 {
+			c.scale = 0.01
+		}
+	}
+}
+
+func (c *Canvas) applyTransforms(gtx layout.Context) {
+	op.Offset(f32.Pt(c.offsetX, c.offsetY)).Add(gtx.Ops)
+	aff := f32.Affine2D{}.Scale(
+		f32.Pt(0, 0),
+		f32.Pt(c.scale, c.scale),
+	)
+	op.Affine(aff).Add(gtx.Ops)
 }
 
 type input struct {
 	pointerEventTag struct{}
 	pointerID       pointer.ID
+	scroll          bool
+	scrollY         float32
 	drag            bool
 	dragDeltaX      float32
 	dragDeltaY      float32
@@ -51,6 +76,7 @@ type input struct {
 }
 
 func (i *input) update(gtx layout.Context) {
+	i.scroll = false
 	for _, evt := range gtx.Events(&i.pointerEventTag) {
 		x, ok := evt.(pointer.Event)
 		if !ok {
@@ -58,6 +84,14 @@ func (i *input) update(gtx layout.Context) {
 		}
 
 		switch x.Type {
+		case pointer.Scroll:
+			if i.pointerID != x.PointerID {
+				break
+			}
+			if x.Scroll.Y != 0 {
+				i.scroll = true
+				i.scrollY = x.Scroll.Y * .02
+			}
 		case pointer.Press:
 			if i.drag {
 				break
@@ -78,11 +112,13 @@ func (i *input) update(gtx layout.Context) {
 			fallthrough
 		case pointer.Cancel:
 			i.drag = false
+			i.scroll = false
 		}
 	}
 
 	pointer.InputOp{Tag: &i.pointerEventTag,
-		Types: pointer.Press | pointer.Drag | pointer.Release,
-		Grab:  i.drag,
+		ScrollBounds: image.Rect(-1, -1, 1, 1),
+		Types:        pointer.Press | pointer.Drag | pointer.Release | pointer.Scroll,
+		Grab:         i.drag,
 	}.Add(gtx.Ops)
 }
